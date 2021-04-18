996316481b133fcf6978f9eb106b071bc01b5602
chenmaochang <chenmaochang@qq.com>
2021-04-18 23:48:00 +0800

    feat: 添加redisson

---
+++ src/main/java/com/cmc/cas/common/config/redis/RedissonConfig.java
@@ -0,0 +1,31 @@
+package com.cmc.cas.common.config.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+
+@Configuration
+public class RedissonConfig {
+
+    @Resource
+    private RedisProperties redisProperties;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        String redisUrl = String.format("redis://%s:%s", redisProperties.getHost() + "", redisProperties.getPort() + "");
+        config.useSingleServer()
+                .setAddress(redisUrl)
+                .setPassword(redisProperties.getPassword())
+                .setTimeout(10000)
+                .setDatabase(0);
+        return Redisson.create(config);
+    }
+
+}
---
+++ src/main/java/com/cmc/cas/common/dto/user/RegisterUserDTO.java
@@ -0,0 +1,8 @@
+package com.cmc.cas.common.dto.user;
+
+import lombok.Data;
+
+@Data
+public class RegisterUserDTO {
+
+}
